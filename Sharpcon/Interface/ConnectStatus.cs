87d3a384eab15ea31301f19e8f33c692b7344d53
Kieran South <kieran.south@hotmail.co.uk>
2017-03-15 13:15:47 +0000

    Remove unused 'using' directives and sample data

--- Sharpcon/Interface/ConnectStatus.cs
+++ Sharpcon/Interface/ConnectStatus.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sharpcon.WebSockets;
-
-namespace Sharpcon.Interface
+﻿namespace Sharpcon.Interface
 {
     public static class ConnectStatus
     {
